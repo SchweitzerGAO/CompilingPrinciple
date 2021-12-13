370e405933869aad8cdfc53997a5a37c4f474c04
Charles Gao <1030587461@qq.com>
2021-12-13 16:12:47 +0800

    quad data structure finished

---
+++ src/com/cp/gotostat/VarList.java
@@ -0,0 +1,4 @@
+package com.cp.gotostat;
+
+public class VarList {
+}
--- src/com/cp/pblc/Quad.java
+++ src/com/cp/pblc/Quad.java
@@ -1,4 +1,60 @@
-package com.cp.pblc;
+package com.cp.pblc;  // public classes
+
 
 public class Quad {
+    private int address;  // address starting from 100
+    private String op;    // operator
+    private String arg1;  // argument 1
+    private String arg2;  // argument 2
+    private int nextHop;  // the next quad to jump
+
+    public Quad(int address, String op, String arg1, String arg2, int nextHop) {
+        this.address = address;
+        this.op = op;
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+        this.nextHop = nextHop;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public String getArg1() {
+        return arg1;
+    }
+
+    public void setArg1(String arg1) {
+        this.arg1 = arg1;
+    }
+
+    public String getArg2() {
+        return arg2;
+    }
+
+    public void setArg2(String arg2) {
+        this.arg2 = arg2;
+    }
+
+    public int getNextHop() {
+        return nextHop;
+    }
+
+    public void setNextHop(int nextHop) {
+        this.nextHop = nextHop;
+    }
+
+
 }

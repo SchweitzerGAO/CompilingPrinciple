86678d123461e16390dd188ad3581a8fc8c1a0e4
LuBixing <1172940427@qq.com>
2022-01-05 21:57:16 +0800

    调整了控制测试的结构

--- src/com/cp/controlstat/ControlStatement.java
+++ src/com/cp/controlstat/ControlStatement.java
@@ -2,6 +2,7 @@ package com.cp.controlstat;
 
 import com.cp.pblc.QuadList;
 import com.cp.pblc.BooleanElem;
+import com.cp.pblc.QuadListNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +16,12 @@ public class ControlStatement {
     //布尔表达式E，里面有两个综合属性E.truelist和E.falselist
     private List<BooleanElem> booleanElemList;
     private QuadList quadList;
-
+    private boolean hasError;
 
     public ControlStatement(){
         quadList = new QuadList();
         booleanElemList = new ArrayList<>();
+        hasError = false;
     }
     public QuadList getQuadList() {
         return quadList;
@@ -59,5 +61,20 @@ public class ControlStatement {
 //            symbolList.add(new SymbolListElem(label,"label",true,quadList.getNextQuad()));
 //            quadList.emit("j","-","-",0);
 //        }
+    }
+
+    public void printQuadList()
+    {
+        if(hasError)
+        {
+            System.err.println("COMPILE ERROR");
+            return;
+        }
+        QuadListNode temp = this.quadList.getHead();
+        while(temp != null)
+        {
+            temp.getData().print();
+            temp = temp.getNext();
+        }
     }
 }
